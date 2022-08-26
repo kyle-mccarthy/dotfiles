719eb1ee513e2b5b42630a0ad28940106f145d51
Kyle McCarthy <kyle@osmos.io>
2022-08-25 22:06:24 -0500

    fix: add prettier and eslint_d back to null-ls config

--- .config/nvim/lua/plugins/null-ls-cfg.lua
+++ .config/nvim/lua/plugins/null-ls-cfg.lua
@@ -3,12 +3,12 @@ local null_ls = require('null-ls')
 null_ls.setup(
   {
     sources = {
-      -- null_ls.builtins.formatting.prettier.with({ prefer_local = true }),
+      null_ls.builtins.formatting.prettier.with({ prefer_local = true }),
       -- null_ls.builtins.formatting.rustywind,
 
       -- eslint
-      -- null_ls.builtins.diagnostics.eslint_d,
-      -- null_ls.builtins.code_actions.eslint_d,
+      null_ls.builtins.diagnostics.eslint_d,
+      null_ls.builtins.code_actions.eslint_d,
       -- null_ls.builtins.formatting.eslint,
       -- null_ls.builtins.diagnostics.eslint_d,
 
